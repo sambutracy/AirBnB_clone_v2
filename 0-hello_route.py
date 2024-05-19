97e162dde8c1b3af9ab92ca0f0104fdd66a1edae
sambutracy <tracysambut@gmail.com>
2024-05-19 16:00:35 -0700

    Add task 0: hello flask

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """
     Displays 'Hello HBNB!' on the main route.
@@ -15,4 +15,5 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!' on the main route.
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
